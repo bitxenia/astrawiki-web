02285efa909523ce88faa00ee5bff3144c0f596c
Joaquin Velazquez <jvelazquez@fi.uba.ar>
2024-12-05 12:56:36 -0300

    add search box in main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,19 @@ import Link from "next/link";
 
 import { buttonVariants } from "@/components/ui/button";
 import { PageRoutes } from "@/lib/pageroutes";
+import Search from "@/components/navigation/search";
 
 export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
       <h1 className="text-4xl font-bold mb-4 sm:text-7xl">
-        Documents
+        Bitxenia
       </h1>
-      <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
-        A simple open-source product documentation platform. That&apos;s simple to use and easy to customize.
-      </p>
+      <h2 className="text-2xl font-bold mb-4 sm:text-4xl">
+        Knowledge Repository
+      </h2>
       <div className="flex items-center gap-5">
-        <Link
-          href={`/docs${PageRoutes[0].href}`}
-          className={buttonVariants({ className: "px-6", size: "lg" })}
-        >
-          Get Started
-        </Link>
+        <Search />
       </div>
     </div>
   );
