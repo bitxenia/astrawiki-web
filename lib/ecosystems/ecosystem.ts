53e0d924347cea7c87d32f55002b8042cedb6f9f
Joaquin Velazquez <jvelazquez@fi.uba.ar>
2024-12-10 23:03:24 -0300

    update ecosystem interface to match readme

--- lib/ecosystems/ecosystem.ts
+++ lib/ecosystems/ecosystem.ts
@@ -1,21 +1,27 @@
+export type Change = string; //TODO
 
-interface Ecosystem {
+export type Article = {
+    name: string;
+    changelog: Change[];
+}
+
+export interface Ecosystem {
     /* Fetches an article given it's name, or null if the article doesn't
      * exist.
     */
-    fetchArticle(name: string): string | null;
-    
+    fetchArticle(name: string): Promise<Article | null>;
+
     /*
-     * Adds article to repository, returns true if it was successful, false
+     * Adds empty article to repository, returns true if it was successful, false
      * otherwise. An article name must be unique.
     */
-    addArticle(name: string, content: string): boolean;
+    addArticle(name: string): Promise<boolean>;
 
     /*
      * Edits an article in a git style, where only the changes are passed to 
      * the method.
     */
-    editArticle(name: string, changes: string[]): boolean;
+    editArticle(name: string, changes: Change[]): Promise<boolean>;
 
     /*
      * Returns all the changes done to an article.
