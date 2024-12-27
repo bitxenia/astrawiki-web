d5931f5978a2cc2824c44c5f03b996073d96e6da
SBen-IV <sinneo@fi.uba.ar>
2024-12-27 14:15:00 -0300

    feat: history view WIP

--- example-server/main.js
+++ example-server/main.js
@@ -1,94 +1,98 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
+import cors from "cors";
 import { readFile, writeFile, access } from "node:fs/promises";
 const __dirname = import.meta.dirname;
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/articles/:name', async (req, res) => {
-    const { name } = req.params;
-    try {
-        const patchesPath = path.join(__dirname, "content", `${name}.json`);
-
-        const patches = await readFile(patchesPath, "utf-8");
-
-        res.status(200).json({ patches });
-    } catch (err) {
-        if (err.code === "ENOENT") {
-            res.status(404).json({ error: "Document not found" });
-        } else {
-            console.log(err);
-            res.status(500).json({ error: "Internal Server Error" });
-        }
-    }
-})
-
-app.post('/articles', async (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        console.log("Name is required");
-        return res.status(400).json({ error: "Name is required" });
-    }
-
+app.get("/articles/:name", async (req, res) => {
+  const { name } = req.params;
+  try {
     const patchesPath = path.join(__dirname, "content", `${name}.json`);
 
-    try {
-        await access(patchesPath);
-        console.log("Article with this name already exists");
-        return res.status(409).json({ error: "Article with this name already exists" });
-    } catch (err) {
-        if (err.code != "ENOENT") {
-            console.log(err);
-            return res.status(500).json({ error: "Internal Server Error" });
-        }
-    }
+    const patches = await readFile(patchesPath, "utf-8");
 
-    try {
-        await writeFile(patchesPath, "[]", "utf-8");
-        console.log("Empty article created successfully");
-        res.status(201).json({ message: "Empty article created successfully" });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Internal Server Error" });
+    res.status(200).json({ patches });
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      res.status(404).json({ error: "Document not found" });
+    } else {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-})
+  }
+});
 
-app.patch('/articles/:name', async (req, res) => {
-    const { name } = req.params;
-    const { date, patch } = req.body;
-    if (!date) {
-        return res.status(400).json({ error: "Date is required" });
-    } else if (!patch) {
-        return res.status(400).json({ error: "Patch is required" });
+app.post("/articles", async (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    console.log("Name is required");
+    return res.status(400).json({ error: "Name is required" });
+  }
+
+  const patchesPath = path.join(__dirname, "content", `${name}.json`);
+
+  try {
+    await access(patchesPath);
+    console.log("Article with this name already exists");
+    return res
+      .status(409)
+      .json({ error: "Article with this name already exists" });
+  } catch (err) {
+    if (err.code != "ENOENT") {
+      console.log(err);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
-    const patchesPath = path.join(__dirname, "content", `${name}.json`);
-    let patches = [];
-    try {
-        patches = JSON.parse(await readFile(patchesPath, "utf-8"));
-    } catch (err) {
-        if (err.code !== "ENOENT") {
-            return res.status(404).json({ error: `File "${patchesPath}" not found` });
-        }
+  }
+
+  try {
+    await writeFile(patchesPath, "[]", "utf-8");
+    console.log("Empty article created successfully");
+    res.status(201).json({ message: "Empty article created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.patch("/articles/:name", async (req, res) => {
+  const { name } = req.params;
+  const { date, patch } = req.body;
+  if (!date) {
+    return res.status(400).json({ error: "Date is required" });
+  } else if (!patch) {
+    return res.status(400).json({ error: "Patch is required" });
+  }
+  const patchesPath = path.join(__dirname, "content", `${name}.json`);
+  let patches = [];
+  try {
+    patches = JSON.parse(await readFile(patchesPath, "utf-8"));
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      return res.status(404).json({ error: `File "${patchesPath}" not found` });
     }
+  }
 
-    patches.push({ date, patch });
+  patches.push({ date, patch });
 
-    try {
-        await writeFile(patchesPath, JSON.stringify(patches), "utf-8");
-        res.status(200).json({ message: "Article updated successfully" });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-})
+  try {
+    await writeFile(patchesPath, JSON.stringify(patches), "utf-8");
+    res.status(200).json({ message: "Article updated successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-app.get('/', async (req, res) => {
-    return res.status(200).json({ message: "Server is up" });
-})
+app.get("/", async (req, res) => {
+  return res.status(200).json({ message: "Server is up" });
+});
 
 app.listen(port, () => {
-    console.log('Example server running on port 3001');
+  console.log("Example server running on port 3001");
 });
--- lib/markdown.ts
+++ lib/markdown.ts
@@ -7,11 +7,11 @@ import rehypePrism from "rehype-prism-plus";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import rehypeCodeTitles from "rehype-code-titles";
-import rehypeKatex from 'rehype-katex'
+import rehypeKatex from "rehype-katex";
 import { visit } from "unist-util-visit";
 
 import { PageRoutes } from "@/lib/pageroutes";
-import { components } from '@/lib/components';
+import { components } from "@/lib/components";
 import { Settings } from "@/lib/meta";
 import { GitHubLink } from "@/settings/navigation";
 import { Ecosystem } from "./ecosystems/ecosystem";
@@ -19,47 +19,47 @@ import { getTextFromPatches } from "./diff";
 import { ReactElement } from "react";
 
 async function parseMdx<Frontmatter>(rawMdx: string) {
-    return await compileMDX<Frontmatter>({
-        source: rawMdx,
-        options: {
-            parseFrontmatter: true,
-            mdxOptions: {
-                rehypePlugins: [
-                    preCopy,
-                    rehypeCodeTitles,
-                    rehypeKatex,
-                    rehypePrism,
-                    rehypeSlug,
-                    rehypeAutolinkHeadings,
-                    postCopy,
-                ],
-                remarkPlugins: [remarkGfm],
-            },
-        },
-        components,
-    });
+  return await compileMDX<Frontmatter>({
+    source: rawMdx,
+    options: {
+      parseFrontmatter: true,
+      mdxOptions: {
+        rehypePlugins: [
+          preCopy,
+          rehypeCodeTitles,
+          rehypeKatex,
+          rehypePrism,
+          rehypeSlug,
+          rehypeAutolinkHeadings,
+          postCopy,
+        ],
+        remarkPlugins: [remarkGfm],
+      },
+    },
+    components,
+  });
 }
 
 type BaseMdxFrontmatter = {
-    title: string;
-    description: string;
-    keywords: string;
+  title: string;
+  description: string;
+  keywords: string;
 };
 
 const computeDocumentPath = (slug: string) => {
-    return Settings.gitload
-        ? `${GitHubLink.href}/raw/main/contents/docs/${slug}/index.mdx`
-        : path.join(process.cwd(), "/contents/docs/", `${slug}/index.mdx`);
+  return Settings.gitload
+    ? `${GitHubLink.href}/raw/main/contents/docs/${slug}/index.mdx`
+    : path.join(process.cwd(), "/contents/docs/", `${slug}/index.mdx`);
 };
 
 const getDocumentPathMemoized = (() => {
-    const cache = new Map<string, string>();
-    return (slug: string) => {
-        if (!cache.has(slug)) {
-            cache.set(slug, computeDocumentPath(slug));
-        }
-        return cache.get(slug)!;
-    };
+  const cache = new Map<string, string>();
+  return (slug: string) => {
+    if (!cache.has(slug)) {
+      cache.set(slug, computeDocumentPath(slug));
+    }
+    return cache.get(slug)!;
+  };
 })();
 
 /**
@@ -88,41 +88,50 @@ export async function parseMarkdown(title: string, rawMd: string): Promise<React
 }
 
 export async function getDocument(slug: string, ecosystem: Ecosystem) {
-    try {
-        // const contentPath = getDocumentPathMemoized(slug);
-        // let rawMdx = "";
-        // let lastUpdated: string | null = null;
-        //
-        // if (Settings.gitload) {
-        //     const response = await fetch(contentPath);
-        //     if (!response.ok) {
-        //         throw new Error(`Failed to fetch content from GitHub: ${response.statusText}`);
-        //     }
-        //     rawMdx = await response.text();
-        //     lastUpdated = response.headers.get('Last-Modified') ?? null;
-        // } else {
-        //     rawMdx = await fs.readFile(contentPath, "utf-8");
-        //     const stats = await fs.stat(contentPath);
-        //     lastUpdated = stats.mtime.toISOString();
-        // }
-        const article = await ecosystem.fetchArticle(slug);
-        const frontmatter = `---\ntitle: ${slug}\n---\n`;
-        const rawMdx = frontmatter.concat(getTextFromPatches(article.patches));
+  try {
+    // const contentPath = getDocumentPathMemoized(slug);
+    // let rawMdx = "";
+    // let lastUpdated: string | null = null;
+    //
+    // if (Settings.gitload) {
+    //     const response = await fetch(contentPath);
+    //     if (!response.ok) {
+    //         throw new Error(`Failed to fetch content from GitHub: ${response.statusText}`);
+    //     }
+    //     rawMdx = await response.text();
+    //     lastUpdated = response.headers.get('Last-Modified') ?? null;
+    // } else {
+    //     rawMdx = await fs.readFile(contentPath, "utf-8");
+    //     const stats = await fs.stat(contentPath);
+    //     lastUpdated = stats.mtime.toISOString();
+    // }
+    const article = await ecosystem.fetchArticle(slug);
+    const frontmatter = `---\ntitle: ${slug}\n---\n`;
+    const rawMdx = frontmatter.concat(getTextFromPatches(article.patches));
 
-        const parsedMdx = await parseMdx<BaseMdxFrontmatter>(rawMdx);
-        // const tocs = await getTable(slug);
-        const tocs: any[] = [];
+    const parsedMdx = await parseMdx<BaseMdxFrontmatter>(rawMdx);
+    // const tocs = await getTable(slug);
+    const tocs: any[] = [];
 
-        return {
-            frontmatter: parsedMdx.frontmatter,
-            content: parsedMdx.content,
-            tocs,
-            lastUpdated: null,
-        };
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
+    return {
+      frontmatter: parsedMdx.frontmatter,
+      content: parsedMdx.content,
+      tocs,
+      lastUpdated: null,
+    };
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+export async function getPatches(slug: string, ecosystem: Ecosystem) {
+  const article = await ecosystem.fetchArticle(slug);
+  // const frontmatter = `---\ntitle: ${slug}\n---\n`;
+
+  return {
+    patches: article.patches,
+  };
 }
 
 // export async function getRawDocument(slug: string) {
@@ -186,36 +195,38 @@ export async function getDocument(slug: string, ecosystem: Ecosystem) {
 //     return text.toLowerCase().replace(/\s+/g, "-").replace(/[^a-z0-9-]/g, "");
 // }
 
-const pathIndexMap = new Map(PageRoutes.map((route, index) => [route.href, index]));
+const pathIndexMap = new Map(
+  PageRoutes.map((route, index) => [route.href, index])
+);
 
 export function getPreviousNext(path: string) {
-    const index = pathIndexMap.get(`/${path}`);
+  const index = pathIndexMap.get(`/${path}`);
 
-    if (index === undefined || index === -1) {
-        return { prev: null, next: null };
-    }
+  if (index === undefined || index === -1) {
+    return { prev: null, next: null };
+  }
 
-    const prev = index > 0 ? PageRoutes[index - 1] : null;
-    const next = index < PageRoutes.length - 1 ? PageRoutes[index + 1] : null;
+  const prev = index > 0 ? PageRoutes[index - 1] : null;
+  const next = index < PageRoutes.length - 1 ? PageRoutes[index + 1] : null;
 
-    return { prev, next };
+  return { prev, next };
 }
 
 const preCopy = () => (tree: any) => {
-    visit(tree, "element", (node) => {
-        if (node.tagName === "pre") {
-            const [codeEl] = node.children;
-            if (codeEl?.tagName === "code") {
-                node.raw = codeEl.children?.[0]?.value || "";
-            }
-        }
-    });
+  visit(tree, "element", (node) => {
+    if (node.tagName === "pre") {
+      const [codeEl] = node.children;
+      if (codeEl?.tagName === "code") {
+        node.raw = codeEl.children?.[0]?.value || "";
+      }
+    }
+  });
 };
 
 const postCopy = () => (tree: any) => {
-    visit(tree, "element", (node) => {
-        if (node.tagName === "pre" && node.raw) {
-            node.properties["raw"] = node.raw;
-        }
-    });
+  visit(tree, "element", (node) => {
+    if (node.tagName === "pre" && node.raw) {
+      node.properties["raw"] = node.raw;
+    }
+  });
 };
