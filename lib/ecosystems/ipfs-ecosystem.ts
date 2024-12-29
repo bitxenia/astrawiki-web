da362cac13c61c6aeadafa11aff915127129ae51
Joaquin Prada <joaquiprada@gmail.com>
2024-12-28 22:43:04 -0300

    initial implementation for creating & editing an article

--- lib/ecosystems/ipfs-ecosystem.ts
+++ lib/ecosystems/ipfs-ecosystem.ts
@@ -2,7 +2,7 @@ import { Article, Ecosystem, Patch } from "./ecosystem";
 import { startOrbitDB } from "./utils/start_orbitdb";
 import { getArticlesDb } from "./utils/get_articles_db";
 import { getArticleContent } from "./utils/get_article_content";
-import { type OrbitDB } from "@orbitdb/core";
+import { type OrbitDB, IPFSAccessController } from "@orbitdb/core";
 
 class IPFSEcosystem implements Ecosystem {
   orbitdb: OrbitDB;
@@ -39,10 +39,29 @@ class IPFSEcosystem implements Ecosystem {
   }
 
   async createArticle(name: string): Promise<null> {
+    // TODO: Check if article already exists
+
+    // TODO: The database needs to stay accessible for the collaborators to replicate it.
+    //       See how to achieve this or change the responsibility of creating the database to
+    //       the collaborators nodes.
+    const newArticleDb = await this.orbitdb.open(name, {
+      AccessController: IPFSAccessController({ write: ["*"] }),
+    });
+
+    let articleAddress = newArticleDb.address.toString();
+    await this.articleDb.add("articleName::" + name + "::" + articleAddress);
+
     return null;
   }
 
   async editArticle(name: string, patch: Patch): Promise<null> {
+    // TODO: We assume that the providers are already connected. We should add a check for this.
+    let articleDb = await this.orbitdb.open(name);
+
+    // TODO: Wait to replicate?
+    // TODO: We should store the patches in a more efficient way.
+    await articleDb.add(JSON.stringify(patch));
+
     return null;
   }
 }
