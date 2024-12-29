0b2a20bf10465189c0ad3d741da3dab80c39e923
Joaquin Prada <joaquiprada@gmail.com>
2024-12-28 22:05:05 -0300

    First implementation of fetchArticle

--- lib/ecosystems/ipfs-ecosystem.ts
+++ lib/ecosystems/ipfs-ecosystem.ts
@@ -1,19 +1,39 @@
 import { Article, Ecosystem, Patch } from "./ecosystem";
 import { startOrbitDB } from "./utils/start_orbitdb";
-import {type OrbitDB} from "@orbitdb/core";
-
+import { getArticlesDb } from "./utils/get_articles_db";
+import { getArticleContent } from "./utils/get_article_content";
+import { type OrbitDB } from "@orbitdb/core";
 
 class IPFSEcosystem implements Ecosystem {
-    orbitdb: OrbitDB;
+  orbitdb: OrbitDB;
+  articleDb: any;
 
-    constructor() {
-        this.orbitdb = startOrbitDB();
-    }
+  constructor() {
+    this.orbitdb = startOrbitDB();
+    this.articleDb = getArticlesDb(this.orbitdb);
+  }
 
   async fetchArticle(name: string): Promise<Article> {
+    // TODO: Implement a better protocol.
+    // Article protocol:
+    // <article-name>::<orbitdb_article_address>
+
+    for await (const record of this.articleDb.iterator()) {
+      let { articleName, articleAddress } = record.payload.value.split("::");
+      if (articleName === name) {
+        let patches = await getArticleContent(this.orbitdb, articleAddress);
+
+        let article: Article = {
+          name: articleName,
+          patches: patches,
+        };
+        return article;
+      }
+    }
+    console.log(`Article ${name} not found`);
     let articulo: Article = {
-      name: "Articulo",
-      patches: []
+      name: "",
+      patches: [],
     };
     return articulo;
   }
---
+++ lib/ecosystems/utils/get_article_content.ts
@@ -0,0 +1,20 @@
+import { type OrbitDB } from "@orbitdb/core";
+import { Patch } from "../ecosystem";
+
+export const getArticleContent = async (
+  orbitdb: OrbitDB,
+  articleAddress: string
+) => {
+  // TODO: We assume that the providers are already connected. We should add a check for this.
+  let db = await orbitdb.open(articleAddress);
+
+  // TODO: Wait for replication to finish?
+
+  // TODO: We should store the patches in a more efficient way.
+  let patches: Patch[] = [];
+  for await (const record of db.iterator()) {
+    let patch = JSON.parse(record.payload.value);
+    patches.push(patch);
+  }
+  return patches;
+};
