5b4fc4e03d72cc8e9a163b8cd439904c34bbd510
SBen-IV <72094271+SBen-IV@users.noreply.github.com>
2024-12-30 10:13:45 -0300

    Feature history view (#27)
    * feat: history view WIP
    
    * feat: view history and older version of an article

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { notFound } from "next/navigation";
 import { getRawArticle, parseMarkdown } from "@/lib/markdown";
 import { Settings } from "@/lib/meta";
@@ -11,61 +11,86 @@ import { BackToTop } from "@/components/navigation/backtotop";
 import { Typography } from "@/components/ui/typography";
 import { ReactElement, useContext, useEffect, useState } from "react";
 import { Ecosystem } from "@/lib/ecosystems/ecosystem";
-import { ArticleContext, ArticleContextProps, EcosystemContext } from "@/lib/contexts";
+import {
+  ArticleContext,
+  ArticleContextProps,
+  EcosystemContext,
+} from "@/lib/contexts";
 import { BarLoader } from "react-spinners";
+import Link from "next/link";
 
 type PageProps = {
-    params: { slug: string[] };
+  params: { slug: string[] };
 };
 
 export default function Pages({ params: { slug = [] } }: PageProps) {
-    const [parsedMarkdown, setParsedMarkdown] = useState<ReactElement<any, any> | null>(null);
-    const [error, setError] = useState<boolean>(false);
-    const ecosystem = useContext<Ecosystem>(EcosystemContext)
-    const { setArticle } = useContext<ArticleContextProps>(ArticleContext);
+  const [parsedMarkdown, setParsedMarkdown] = useState<ReactElement<
+    any,
+    any
+  > | null>(null);
+  const [error, setError] = useState<boolean>(false);
+  const ecosystem = useContext<Ecosystem>(EcosystemContext);
+  const { setArticle } = useContext<ArticleContextProps>(ArticleContext);
 
-    const pathName = slug.join("/");
-    useEffect(() => {
-        async function fetchDocument() {
-            try {
-                const rawArticle = await getRawArticle(pathName, ecosystem);
-                const res = await parseMarkdown(pathName, rawArticle);
-                if (!res) {
-                    setError(true);
-                } else {
-                    setParsedMarkdown(res);
-                    setArticle(rawArticle);
-                }
-            } catch {
-                setError(true);
-            }
+  const pathName = slug[0];
+  let articleVersion: number | null = null;
+  if (slug.length > 1) {
+    articleVersion = +slug[1];
+  }
+
+  useEffect(() => {
+    async function fetchDocument() {
+      try {
+        const rawArticle = await getRawArticle(
+          pathName,
+          ecosystem,
+          articleVersion
+        );
+        const res = await parseMarkdown(pathName, rawArticle);
+        if (!res) {
+          setError(true);
+        } else {
+          setParsedMarkdown(res);
+          setArticle(rawArticle);
         }
-        fetchDocument();
-    }, [pathName, ecosystem, setArticle]);
+      } catch {
+        setError(true);
+      }
+    }
+    fetchDocument();
+  }, [pathName, ecosystem, setArticle]);
 
-    if (error) notFound();
+  if (error) notFound();
 
-    return (
-        <div className="flex items-start gap-14">
-            <div className="flex-[3] pt-10">
-                <PageBreadcrumb paths={slug} />
-                {parsedMarkdown && <Typography>
-                    <h1 className="text-3xl -mt-2">{pathName}</h1>
-                    <div>{parsedMarkdown}</div>
-                    <Pagination pathname={pathName} />
-                </Typography>}
-                {!parsedMarkdown && <div className="flex justify-center items-center min-h-screen">
-                    <BarLoader />
-                </div>}
-            </div>
-            {parsedMarkdown && Settings.rightbar && (
-                <div className="hidden xl:flex xl:flex-col sticky top-16 gap-3 py-8 min-w-[230px] h-[94.5vh] toc">
-                    {Settings.toc && <Toc tocs={[]} />}
-                    {Settings.feedback && <Feedback slug={pathName} title={pathName} />}
-                    {Settings.totop && <BackToTop className="mt-6 self-start text-sm text-neutral-800 dark:text-neutral-300/85" />}
-                </div>
-            )}
+  return (
+    <div className="flex items-start gap-14">
+      <div className="flex-[3] pt-10">
+        <PageBreadcrumb paths={slug} />
+        <div>
+          <Link href={"/docs/history/" + slug[0]}>History</Link>
         </div>
-    );
+        {parsedMarkdown && (
+          <Typography>
+            <h1 className="text-3xl -mt-2">{pathName}</h1>
+            <div>{parsedMarkdown}</div>
+            <Pagination pathname={pathName} />
+          </Typography>
+        )}
+        {!parsedMarkdown && (
+          <div className="flex justify-center items-center min-h-screen">
+            <BarLoader />
+          </div>
+        )}
+      </div>
+      {parsedMarkdown && Settings.rightbar && (
+        <div className="hidden xl:flex xl:flex-col sticky top-16 gap-3 py-8 min-w-[230px] h-[94.5vh] toc">
+          {Settings.toc && <Toc tocs={[]} />}
+          {Settings.feedback && <Feedback slug={pathName} title={pathName} />}
+          {Settings.totop && (
+            <BackToTop className="mt-6 self-start text-sm text-neutral-800 dark:text-neutral-300/85" />
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
-
---
+++ app/docs/history/[[...slug]]/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { EcosystemContext } from "@/lib/contexts";
+import { Ecosystem, Patch } from "@/lib/ecosystems/ecosystem";
+import { getDocument, getPatches } from "@/lib/markdown";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+
+type PageProps = {
+  params: { slug: string[] };
+};
+
+export default function Pages({ params: { slug = [] } }: PageProps) {
+  const [document, setDocument] = useState<any>(null);
+  const [error, setError] = useState<boolean>(false);
+  const ecosystem = useContext<Ecosystem>(EcosystemContext);
+
+  const pathName = slug.join("/");
+  useEffect(() => {
+    async function fetchDocument() {
+      try {
+        const res = await getPatches(pathName, ecosystem);
+        if (!res) {
+          setError(true);
+        } else {
+          setDocument(res);
+        }
+      } catch {
+        setError(true);
+      }
+    }
+    fetchDocument();
+  }, [pathName, ecosystem]);
+
+  if (error) notFound();
+
+  if (!document) {
+    return <p> Loading... </p>;
+  }
+
+  const { patches } = document;
+
+  return (
+    <div className="flex items-start gap-14">
+      <ul>
+        {patches
+          .sort((a: Patch, b: Patch) => {
+            if (a.date > b.date) return -1;
+            if (a.date < b.date) return 1;
+            return 0;
+          })
+          .map((p: Patch, i: number) => (
+            <li className="py-2">
+              <Link
+                href={
+                  "/docs/" +
+                  slug.toString() +
+                  "/" +
+                  (patches.length - i).toString()
+                }
+              >
+                {patches.length - i} - {p.date}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
