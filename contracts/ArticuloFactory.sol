079de09c31463f22bef5bd66c7ec430dfb84b885
Lucas Sotelo <lucasnsotelog@gmail.com>
2025-01-14 23:22:42 -0300

    feat: lista de articulos eth

--- contracts/ArticuloFactory.sol
+++ contracts/ArticuloFactory.sol
@@ -5,6 +5,7 @@ import "./Articulo.sol";
 
 contract ArticuloFactory {
     address[] public articulos;
+    string[] public titulos;
     mapping(string => address) public tituloToAddress;
 
     function crearArticulo(
@@ -14,9 +15,14 @@ contract ArticuloFactory {
         address newArticulo = address(new Articulo(titulo, contenido));
         tituloToAddress[titulo] = newArticulo;
         articulos.push(newArticulo);
+        titulos.push(titulo);
     }
 
     function getArticulos() public view returns (address[] memory) {
         return articulos;
+    }
+
+    function getTitulos() public view returns (string[] memory) {
+        return titulos;
     }
 }
