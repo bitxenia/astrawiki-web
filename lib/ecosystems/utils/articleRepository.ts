3b18423519b577f5e6d03f052b46f81664a9e993
Joaquin Prada <joaquiprada@gmail.com>
2025-02-08 02:21:35 +0000

    connection by finding auto-tls wss multiaddr on the dht working

--- lib/ecosystems/utils/articleRepository.ts
+++ lib/ecosystems/utils/articleRepository.ts
@@ -92,9 +92,8 @@ export class ArticleRepository {
   }
 
   private async openArticleRepositoryDB() {
-    // await this.connectToProviders(this.orbitdb.ipfs);
-
-    await this.connectToDebugProvider(this.orbitdb.ipfs);
+    await this.connectToProviders(this.orbitdb.ipfs);
+    // await this.connectToDebugProvider(this.orbitdb.ipfs);
 
     console.log("Attempting to open article repository database...");
     const db = await this.orbitdb.open(DB_ADDRESS);
@@ -107,17 +106,6 @@ export class ArticleRepository {
     });
 
     return db;
-  }
-
-  private async connectToDebugProvider(helia: HeliaLibp2p) {
-    // Local debug peer addr
-    const PEER_ADDRESS_DEBUG =
-      "/ip4/127.0.0.1/tcp/4002/ws/p2p/12D3KooWQrHuEaq3p71Lb5kV5f8VzeqVsnTvisvSKbtuqCzgV699";
-
-    await helia.libp2p
-      .dial(multiaddr(PEER_ADDRESS_DEBUG))
-      .catch(() => console.log(`Cannot dial "${PEER_ADDRESS_DEBUG}"`)); // TODO: Raise error?
-    console.log("Successfully dialed peer address");
   }
 
   private async connectToProviders(helia: HeliaLibp2p) {
@@ -138,28 +126,9 @@ export class ArticleRepository {
           // multiaddrs found
           console.log("Multiaddrs:", provider.multiaddrs.toString());
 
-          // TODO: For some reason the provider multiaddrs are not containing the announced ipv4 addresses.
-          //       For that reason we can not use this solution either until we find a way to get the ipv4 address.
-
-          // TODO: Because of the error detail in the issue below, we need to construct the multiaddr manually.
-          //       We find the secure ipv6 websocket address in the multiaddrs and use it to create the ipv4 address.
-          //       This is a workaround and should be fixed in the future.
-          //       Issue:
-          //       https://github.com/libp2p/js-libp2p/issues/2929
-
-          // We need to convert this (first finding this 2 from all the multiaddrs):
-          // /ip6/<ipv6_addr>/tcp/44644/tls/sni/<ipv6_addr>.k51qzi5uqu5dlr08szssa4nmrja2lh5kcaoac0t3os07p5zoy6szu4xkae5scc.libp2p.direct/ws/p2p/<peer_id>
-          // And this:
-          // /ip4/<public_ipv4_addr>/tcp/4001/ws/p2p/<peer_id>
-          // To this:
-          // /ip4/<public_ipv4_addr>/tcp/4001/tls/sni/<public_ipv4_addr>.k51qzi5uqu5dlr08szssa4nmrja2lh5kcaoac0t3os07p5zoy6szu4xkae5scc.libp2p.direct/ws/p2p/<peer_id>
-          let multiaddrs = provider.multiaddrs.toString().split(",");
-          let ipv4AddrConstructed =
-            await this.contructSecureIpv4WebsocketAddr(multiaddrs);
-
           // Connect to the provider
           try {
-            await helia.libp2p.dial(multiaddr(ipv4AddrConstructed));
+            await helia.libp2p.dial(provider.multiaddrs);
           } catch (err) {
             console.error(err);
             continue;
@@ -182,72 +151,14 @@ export class ArticleRepository {
     }
   }
 
-  private async contructSecureIpv4WebsocketAddr(peerMultiaddrs: string[]) {
-    // TODO: This is a workaround and should be fixed & removed in the future.
+  private async connectToDebugProvider(helia: HeliaLibp2p) {
+    // Local debug peer addr
+    const PEER_ADDRESS_DEBUG =
+      "/ip4/127.0.0.1/tcp/4002/ws/p2p/12D3KooWQrHuEaq3p71Lb5kV5f8VzeqVsnTvisvSKbtuqCzgV699";
 
-    // Find the secure ipv6 websocket address
-    let secureIpv6WebsocketAddr;
-    for (const addr of peerMultiaddrs) {
-      if (addr.includes("tls/sni")) {
-        secureIpv6WebsocketAddr = addr;
-        break;
-      }
-    }
-    if (!secureIpv6WebsocketAddr) {
-      throw new Error("No secure ipv6 websocket address found for provider");
-    }
-
-    // Find the public ipv4 address
-    let ipv4Addr;
-    for (let addr of peerMultiaddrs) {
-      // Remove the peer id part of the address.
-      addr = addr.split("/p2p/")[0];
-
-      if (addr.includes("ip6")) {
-        continue;
-      }
-      if (addr.includes("ws") == false) {
-        continue;
-      }
-      // If the address is a local address, skip it
-      // every addr that starts with 127, 10, 100, 192 are local addresses
-      let ip = addr.split("/")[2].split(".")[0];
-      let startswith = ip.split(".")[0];
-      if (
-        startswith == "127" ||
-        startswith == "10" ||
-        startswith == "100" ||
-        startswith == "192"
-      ) {
-        continue;
-      }
-      ipv4Addr = addr;
-      break;
-    }
-    if (!ipv4Addr) {
-      throw new Error("No publicly routable IPv4 address found for provider");
-    }
-
-    // Construct the ipv4 address
-    let ipv4AddrParts = ipv4Addr.split("/");
-    // Remove the ws part wich is the 5th part
-    ipv4AddrParts.splice(5, 1);
-
-    let ipv6AddrParts = secureIpv6WebsocketAddr.split("/");
-
-    let domain = ipv6AddrParts[7];
-    // Replace the ipv6 with ipv4 in the domain
-    let domainParts = domain.split(".");
-    domainParts[0] = ipv4AddrParts[2].split(".").join("-");
-    domain = domainParts.join(".");
-
-    // Construct the ipv4 address
-    ipv6AddrParts[7] = domain;
-    // Add from the port to the end
-    let ipv4AddrConstructed = ipv4AddrParts.join("/");
-    ipv4AddrConstructed += "/" + ipv6AddrParts.slice(5).join("/");
-
-    console.log("Constructed ipv4 address:", ipv4AddrConstructed);
-    return ipv4AddrConstructed;
+    await helia.libp2p
+      .dial(multiaddr(PEER_ADDRESS_DEBUG))
+      .catch(() => console.log(`Cannot dial "${PEER_ADDRESS_DEBUG}"`)); // TODO: Raise error?
+    console.log("Successfully dialed peer address");
   }
 }
