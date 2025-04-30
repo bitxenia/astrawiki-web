dbdb0f600e6051677a1768aec6d6edc0979126c7
Lucas Sotelo <lucasnsotelog@gmail.com>
2025-04-30 20:04:39 -0300

    feat: create chat at article creation

--- lib/chat/chat-storage.ts
+++ lib/chat/chat-storage.ts
@@ -1,6 +1,8 @@
 import { ChatMessage } from "@bitxenia/astrachat-eth";
 
 export interface ChatStorage {
+  createChat(chatName: string): Promise<void>;
+
   listenToNewMessages(
     chatName: string,
     callback: (message: ChatMessage) => void,
